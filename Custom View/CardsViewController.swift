867ef07b1540d60b430cae973d5f6f215fd2f124
Yinrong "David" Liang <liang162@purdue.edu>
2017-04-03 22:35:47 -0400

    Initial Commit

---
+++ Custom View/CardsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CardsViewController.swift
+//  Custom View
+//
+//  Created by Yinrong Liang on 4/3/17.
+//  Copyright © 2017 Yinrong Liang. All rights reserved.
+//
+
+import UIKit
+
+class CardsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
