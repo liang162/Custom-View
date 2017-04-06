025332d3b594c20b12303e316a66ac97b6f35455
Yinrong "David" Liang <liang162@purdue.edu>
2017-04-06 13:04:07 -0400

    done

--- Custom View/CardsViewController.swift
+++ Custom View/CardsViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class CardsViewController: UIViewController {
-
+    
+    var cardInitialCenter: CGPoint!
+    
+    @IBOutlet weak var profileView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +25,42 @@ class CardsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    var firstTouch: CGPoint!
+    @IBAction func didDrag(_ sender: UIPanGestureRecognizer) {
+        
+        let translation = sender.translation(in: view)
+        firstTouch = sender.location(in: view)
+        print(firstTouch)
+        
+        if(sender.state == .began) {
+            cardInitialCenter = profileView.center
+        } else if(sender.state == .changed) {
+            if(firstTouch.y < profileView.frame.height/2 ) {
+                profileView.center = CGPoint(x: cardInitialCenter.x + translation.x, y: cardInitialCenter.y)
+                profileView.transform = CGAffineTransform(rotationAngle: CGFloat(Double(translation.x) * M_PI / 560))
+            } else {
+                profileView.center = CGPoint(x: cardInitialCenter.x + translation.x, y: cardInitialCenter.y)
+                profileView.transform = CGAffineTransform(rotationAngle: CGFloat(-1.0 * Double(translation.x) * M_PI / 560))
+            }
+            
+            if(translation.x > 50 || translation.x < -50) {
+                UIView.animate(withDuration: 0.4, animations: { 
+                    self.profileView.alpha = 0
+                })
+            }
+        } else if(sender.state == .ended) {
+            profileView.center = CGPoint(x: cardInitialCenter.x, y: cardInitialCenter.y)
+            profileView.transform = CGAffineTransform(rotationAngle: CGFloat(0.0))
+        }
     }
-    */
+
+    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+        performSegue(withIdentifier: "profileSegue", sender: sender)
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationViewController = segue.destination as! ProfileViewController
+        destinationViewController.avatarImageView = profileView
+    }
 
 }
